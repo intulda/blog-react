e9d6c9f86fffc335d1018d01f4214faf927e6dd7
kimbogeun <intulda@naver.com>
2021-01-28 08:37:13 +0900

    app.js변경

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.use(session({
   proxy: true,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production' && true,
+    secure: true,
     domain: process.env.NODE_ENV === 'production' && '.bogeun.dev'
   }
 }));
